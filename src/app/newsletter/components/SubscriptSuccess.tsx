78d9f1fc34b31fef9b3659e7a36fd67f651e1413
leticiagouvea <letfgouvea@gmail.com>
2024-04-28 16:26:32 -0300

    style: adjustments to the newsletter signup page

--- src/app/newsletter/components/SubscriptSuccess.tsx
+++ src/app/newsletter/components/SubscriptSuccess.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import styled from "styled-components";
 import BackgroundForms from "../../components/BackgroundForms";
+import { FaCheck } from "react-icons/fa";
 
 export default function NewsletterSubscript() {
   return (
     <BackgroundForms>
       <Form>
         <h1> Inscrição feita com sucesso!</h1>
+        <FaCheck className="check-icon" />
         <h2> Aguarde... </h2>
         <h3> Entraremos em contato com você por e-mail!</h3>
       </Form>
@@ -16,13 +18,17 @@ export default function NewsletterSubscript() {
 }
 
 const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
   h1 {
     font-weight: 700;
-    font-size: 35px;
+    font-size: 24px;
     color: #423f3f;
     text-align: center;
-    margin-bottom: 25px;
-    margin-top: 30px;
+    margin: 20px 0px 10px 0px;
   }
 
   h2 {
@@ -30,14 +36,20 @@ const Form = styled.div`
     font-size: 24px;
     color: #423f3f;
     text-align: center;
-    margin-bottom: 30px;
+    margin-bottom: 20px;
   }
 
   h3 {
     font-weight: 400;
-    font-size: 27px;
+    font-size: 20px;
     color: #534f4d;
     text-align: center;
-    margin-bottom: 0px;
+    margin-bottom: 10px;
+  }
+
+  .check-icon {
+    color: green;
+    font-size: 30px;
+    margin: 20px 0px;
   }
 `;
