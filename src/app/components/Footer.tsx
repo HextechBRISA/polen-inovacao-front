bf7f396e9ed7383b676e3cdbac567b2fab9dbba6
leticiagouvea <letfgouvea@gmail.com>
2024-07-09 03:45:16 -0300

    style: adjustments to the footer and banner on the home page

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,21 +2,34 @@ import React from "react";
 import { TiSocialInstagram } from "react-icons/ti";
 import { MdFacebook, MdEmail } from "react-icons/md";
 import { RiChatSmile2Fill } from "react-icons/ri";
+import Image from "next/image";
 
 export function Footer() {
   return (
-    <div className="w-full h-[120px] flex justify-around items-center bg-gradient-to-t from-[rgba(255,255,255,0.5)] to-transparent p-10 xs:p-6 mt-[70px] xs:mt-[40px] text-white">
-      <h1 className="font-sans font-bold cursor-pointer hover:text-[#ea5e53] transition duration-300">
-        SOBRE NÓS
-      </h1>
-      <h1 className="font-sans font-bold cursor-pointer sm:hidden hover:text-[#ea5e53] transition duration-300">
-        PÓLEN INOVAÇÃO | 2024 ©
-      </h1>
+    <div className="w-full h-[130px] flex justify-around items-center bg-gradient-to-t from-[rgba(255,255,255,0.5)] to-transparent p-10 xs:p-6 mt-[70px] xs:mt-[40px] text-white">
+      <div className="flex flex-col items-start justify-center text-[15px] font-sans font-bold cursor-pointer sm:hidden">
+        <p className="hover:text-[#d2534a] hvr-forward transition duration-300 mb-1">
+          SOBRE NÓS
+        </p>
+        <p className="hover:text-[#d2534a] hvr-forward transition duration-300 mb-1">
+          FAVELA INOVA
+        </p>
+        <p className="hover:text-[#d2534a] hvr-forward transition duration-300">
+          PARCEIROS
+        </p>
+      </div>
+      <Image
+        src="/LogoPolen.png"
+        alt="Pólen"
+        width={80}
+        height={80}
+        className="xs:w-[70px] xs:h-[60px]"
+      />
       <div className="flex">
-        <RiChatSmile2Fill className="text-[25px] cursor-pointer hover:text-[#ea5e53] transition duration-300" />
-        <TiSocialInstagram className="text-[25px] ml-2 cursor-pointer hover:text-[#ea5e53] transition duration-300" />
-        <MdFacebook className="text-[25px] ml-2 cursor-pointer hover:text-[#ea5e53] transition duration-300" />
-        <MdEmail className="text-[25px] ml-2 cursor-pointer hover:text-[#ea5e53] transition duration-300" />
+        <RiChatSmile2Fill className="text-[25px] cursor-pointer hover:text-[#d2534a] transition duration-300" />
+        <TiSocialInstagram className="text-[25px] ml-2 cursor-pointer hover:text-[#d2534a] transition duration-300" />
+        <MdFacebook className="text-[25px] ml-2 cursor-pointer hover:text-[#d2534a] transition duration-300" />
+        <MdEmail className="text-[25px] ml-2 cursor-pointer hover:text-[#d2534a] transition duration-300" />
       </div>
     </div>
   );
