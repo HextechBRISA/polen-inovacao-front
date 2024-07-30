6f309419d2bb117ec63b27f04ba7cdc76bc87946
leticiagouvea <letfgouvea@gmail.com>
2024-07-30 01:29:46 -0300

    style: pagination effect in event carousel

--- src/app/components/TestimonyCarousel.tsx
+++ src/app/components/TestimonyCarousel.tsx
@@ -26,33 +26,40 @@ const images = [
     src: Testimony3,
     alt: "Depoimento 3",
     name: "Ana Carolina, Circuito Kids",
-    text: "Participar da primeira edição do Favela Inova Niterói me proporcionou uma experiência única não só pelo conteúdo oferecido, mas também pela valorização de startups que se originaram nas comunidades da cidade. A equipe Pólen trabalhou incansavelmente para buscar resolver qualquer problema que surgisse pelo caminho! Como fundadora do Circuito Kids posso afirmar que o Favela Inova trouxe uma outra visão sobre empreendedorismo.",
+    text: "Participar da primeira edição do Favela Inova Niterói me proporcionou uma experiência única não só pelo conteúdo oferecido, mas pela valorização de startups que se originaram nas comunidades da cidade. A equipe Pólen trabalhou para buscar resolver qualquer problema que surgisse pelo caminho! Como fundadora do Circuito Kids posso afirmar que o Favela Inova trouxe uma outra visão sobre empreendedorismo.",
   },
 ];
 
 export default function TestimonyCarousel() {
   return (
     <Swiper
-      direction="vertical"
-      pagination={{ clickable: true }}
+      direction="horizontal"
+      pagination={{
+        clickable: true,
+        renderBullet: (index, className) => {
+          return `<span class="${className} testimony-bullet">${
+            index + 1
+          }</span>`;
+        },
+      }}
       mousewheel={true}
       modules={[Pagination, Mousewheel]}
-      className="mySwiper w-[80%] sm:w-[90%] h-[350px] sm:h-[650px] mx-auto rounded-[10px] mt-12 sm:mt-8 bg-[#eadcd3] bg-opacity-50 shadow-md p-4"
+      className="testimonySwiper w-[80%] sm:w-[90%] h-[350px] sm:h-[630px] mx-auto rounded-[10px] mt-12 sm:mt-8 bg-[#eadcd3] bg-opacity-50 shadow-md p-4"
     >
       {images.map((image, index) => (
         <SwiperSlide key={index}>
-          <div className="relative w-full h-full flex sm:flex-col sm:items-center justify-center">
+          <div className="relative w-full h-full flex sm:flex-col justify-center sm:items-center">
             <Image
               src={image.src}
               alt={image.alt}
               className="w-auto sm:w-56 object-cover mr-6 sm:mr-0 sm:mb-2 border-2"
             />
-            <div className="w-[380px] xs:w-[300px] flex flex-col items-center justify-center">
+            <div className="w-[380px] xs:w-[300px] h-[90%] xs:h-auto flex flex-col items-center justify-center xs:justify-start">
               <p className="text-[16px] flex flex-wrap text-justify leading-tight">
-                <FaQuoteLeft className="text-xl m-2 text-[#ea5e53]" />
+                <FaQuoteLeft className="text-xl m-2 xs:mt-0 text-[#ea5e53]" />
                 {image.text}
               </p>
-              <h3 className="text-[18px] text-[#ea5e53] mt-4 font-bold">
+              <h3 className="text-[18px] text-[#ea5e53] mt-4 xs:mb-4 font-bold">
                 — {image.name}
               </h3>
             </div>
@@ -61,21 +68,24 @@ export default function TestimonyCarousel() {
       ))}
 
       <style jsx global>{`
-        .swiper-pagination-bullet {
+        .testimonySwiper .swiper-pagination {
+          position: absolute;
+          bottom: 4px;
+          left: 0;
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          z-index: 10;
+        }
+        .testimonySwiper .swiper-pagination-bullet {
           background-color: #ee7a3c !important;
-          width: 10px;
-          height: 10px;
+          width: 20px;
+          height: 20px;
+          margin: 10px 8px !important;
+          color: white;
         }
-
-        .swiper-pagination-bullet-active {
+        .testimonySwiper .swiper-pagination-bullet-active {
           background-color: #ea5e53 !important;
-        }
-
-        @media (max-width: 640px) {
-          .swiper-pagination-bullet {
-            width: 10px;
-            height: 10px;
-          }
         }
       `}</style>
     </Swiper>
