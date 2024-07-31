04cb7caec4b2ae39ab9ebf206a557622be9aa6ad
leticiagouvea <letfgouvea@gmail.com>
2024-07-30 22:42:53 -0300

    feat: implement bank transfer option in payment route

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -11,10 +11,16 @@ import {
   ExpiryInput,
   CVCInput,
   PaymentPix,
+  PaymentDeposit,
 } from "./style";
 import PaymentSuccess from "./components/PaymentSuccess";
 import { cardSchema } from "./validationPaymentSchema";
 import { z } from "zod";
+import Image from "next/image";
+import PaymentIcon from "../../../public/PaymentIcon.png";
+import { FaRegCopy } from "react-icons/fa";
+import { CSSTransition } from "react-transition-group";
+import Swal from "sweetalert2";
 
 export default function PaymentPage() {
   const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
@@ -26,7 +32,7 @@ export default function PaymentPage() {
     focus: "" as Focused,
   });
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
-  const [payWithPix, setPayWithPix] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState<string | null>(null);
   const [pixData, setPixData] = useState({
     chave: "sua-chave-pix",
     valor: "00.00",
@@ -59,7 +65,18 @@ export default function PaymentPage() {
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    if (payWithPix) {
+    if (!paymentMethod) {
+      Swal.fire({
+        icon: "error",
+        title: "Erro",
+        text: "Você precisa selecionar uma opção de pagamento!",
+        confirmButtonText: "Ok",
+        confirmButtonColor: "#EA5E53",
+      });
+      return;
+    }
+
+    if (paymentMethod === "pix" || paymentMethod === "deposit") {
       setIsPaymentSuccessful(true);
       return;
     }
@@ -88,12 +105,42 @@ export default function PaymentPage() {
 
   const pixQRCodeData = `00020126580014BR.GOV.BCB.PIX0114${pixData.chave}5204000053039865802BR5925${pixData.recebedor}6009SAO PAULO61080540900062070503***6304`;
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(pixQRCodeData).then(
+      () => {
+        Swal.fire({
+          icon: "success",
+          title: "Código PIX copiado!",
+          text: "O código PIX foi copiado para a área de transferência.",
+          confirmButtonText: "OK",
+          confirmButtonColor: "#EA5E53",
+        });
+      },
+      (err) => {
+        Swal.fire({
+          icon: "error",
+          title: "Falha ao copiar",
+          text: "Não foi possível copiar o código PIX. Por favor, tente novamente.",
+          confirmButtonText: "OK",
+          confirmButtonColor: "#EA5E53",
+        });
+      }
+    );
+  };
+
   return (
     <>
       {isPaymentSuccessful ? (
         <PaymentSuccess />
       ) : (
         <PaymentContainer>
+          <Image
+            src={PaymentIcon}
+            width={80}
+            height={80}
+            alt="Payment Icon"
+            className="mb-6"
+          />
           <h1 className="font-bold text-center text-xl mb-2">
             Realize o pagamento para confirmar sua reserva
           </h1>
@@ -102,95 +149,58 @@ export default function PaymentPage() {
             Valor da reserva: R$ {pixData.valor}
           </h2>
 
-          <PaymentCard>
-            {payWithPix ? (
-              <div className="flex items-center justify-center mb-4">
-                <QRCode value={pixQRCodeData} size={200} />
-              </div>
-            ) : (
-              <>
-                <h3 className="font-normal text-base mb-2">
-                  Pagar com cartão:
-                </h3>
-                <Cards
-                  cvc={cardData.cvc}
-                  expiry={cardData.expiry}
-                  focused={cardData.focus}
-                  name={cardData.name}
-                  number={cardData.number}
-                />
-              </>
-            )}
+          <div className="text-center">
+            <div className="w-[300px] xs:w-full text-left mb-6 flex justify-start items-center">
+              <input
+                type="checkbox"
+                id="cardRadio"
+                name="paymentMethod"
+                className="mr-2 group"
+                checked={paymentMethod === "card"}
+                onChange={() => setPaymentMethod("card")}
+              />
+              <label htmlFor="cardRadio" className="cursor-pointer checkbox">
+                Pagar com cartão
+              </label>
+            </div>
 
-            <form className="w-72 mt-4" onSubmit={handleSubmit}>
-              {!payWithPix && (
-                <div className="mb-1">
-                  <InputMask
-                    mask="9999 9999 9999 9999"
-                    maskChar=""
-                    value={cardData.number}
-                    onChange={handleInputChange}
-                    onFocus={handleInputFocus}
-                  >
-                    {() => (
-                      <div>
-                        <Input
-                          type="tel"
-                          name="number"
-                          placeholder="Número do cartão"
-                          required
-                        />
-                        {errors.number && (
-                          <p className="text-[#EA5E53] font-bold text-sm">
-                            {errors.number}
-                          </p>
-                        )}
-                      </div>
-                    )}
-                  </InputMask>
-                </div>
-              )}
-
-              {!payWithPix && (
-                <div className="mb-1">
-                  <Input
-                    type="text"
-                    name="name"
-                    placeholder="Nome no cartão"
-                    value={cardData.name}
-                    onChange={handleInputChange}
-                    onFocus={handleInputFocus}
-                    required
+            <CSSTransition
+              in={paymentMethod === "card"}
+              timeout={300}
+              classNames="payment-transition"
+              unmountOnExit
+            >
+              <PaymentCard>
+                <div className="card-container">
+                  <Cards
+                    cvc={cardData.cvc}
+                    expiry={cardData.expiry}
+                    focused={cardData.focus}
+                    name={cardData.name}
+                    number={cardData.number}
                   />
-                  {errors.name && (
-                    <p className="text-[#EA5E53] font-bold text-sm">
-                      {errors.name}
-                    </p>
-                  )}
                 </div>
-              )}
 
-              {!payWithPix && (
-                <div className="flex mb-4">
-                  <div className="mr-2">
+                <form className="w-72 mt-4" onSubmit={handleSubmit}>
+                  <div className="mb-1">
                     <InputMask
-                      mask="99/99"
+                      mask="9999 9999 9999 9999"
                       maskChar=""
-                      value={cardData.expiry}
+                      value={cardData.number}
                       onChange={handleInputChange}
                       onFocus={handleInputFocus}
                     >
                       {() => (
                         <div>
-                          <ExpiryInput
+                          <Input
                             type="tel"
-                            name="expiry"
-                            placeholder="Validade"
+                            name="number"
+                            placeholder="Número do cartão"
                             required
                           />
-                          {errors.expiry && (
+                          {errors.number && (
                             <p className="text-[#EA5E53] font-bold text-sm">
-                              {errors.expiry}
+                              {errors.number}
                             </p>
                           )}
                         </div>
@@ -198,52 +208,142 @@ export default function PaymentPage() {
                     </InputMask>
                   </div>
 
-                  <div>
-                    <InputMask
-                      mask="999"
-                      maskChar=""
-                      value={cardData.cvc}
+                  <div className="mb-1">
+                    <Input
+                      type="text"
+                      name="name"
+                      placeholder="Nome no cartão"
+                      value={cardData.name}
                       onChange={handleInputChange}
                       onFocus={handleInputFocus}
-                    >
-                      {() => (
-                        <div>
-                          <CVCInput
-                            type="tel"
-                            name="cvc"
-                            placeholder="CVC"
-                            value={cardData.cvc}
-                            onChange={handleInputChange}
-                            onFocus={handleInputFocus}
-                            required
-                          />
-                          {errors.cvc && (
-                            <p className="text-[#EA5E53] font-bold text-sm">
-                              {errors.cvc}
-                            </p>
-                          )}
-                        </div>
-                      )}
-                    </InputMask>
+                      required
+                    />
+                    {errors.name && (
+                      <p className="text-[#EA5E53] font-bold text-sm">
+                        {errors.name}
+                      </p>
+                    )}
                   </div>
-                </div>
-              )}
-            </form>
-          </PaymentCard>
 
-          <PaymentPix>
-            <input
-              type="checkbox"
-              id="pixCheckbox"
-              className="group"
-              onChange={(e) => setPayWithPix(e.target.checked)}
-            />
-            <label
-              htmlFor="pixCheckbox"
-              className="checkbox cursor-pointer"
-              data-label="Pagar com PIX! (Seu pagamento deverá ser confirmado em até 24h)"
-            ></label>
-          </PaymentPix>
+                  <div className="flex justify-center xs:w-full mb-4">
+                    <div className="mr-2">
+                      <InputMask
+                        mask="99/99"
+                        maskChar=""
+                        value={cardData.expiry}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                      >
+                        {() => (
+                          <>
+                            <ExpiryInput
+                              type="tel"
+                              name="expiry"
+                              placeholder="Validade"
+                              required
+                            />
+                            {errors.expiry && (
+                              <p className="text-[#EA5E53] font-bold text-sm">
+                                {errors.expiry}
+                              </p>
+                            )}
+                          </>
+                        )}
+                      </InputMask>
+                    </div>
+
+                    <div>
+                      <InputMask
+                        mask="999"
+                        maskChar=""
+                        value={cardData.cvc}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                      >
+                        {() => (
+                          <>
+                            <CVCInput
+                              type="tel"
+                              name="cvc"
+                              placeholder="CVC"
+                              value={cardData.cvc}
+                              onChange={handleInputChange}
+                              onFocus={handleInputFocus}
+                              required
+                            />
+                            {errors.cvc && (
+                              <p className="text-[#EA5E53] font-bold text-sm">
+                                {errors.cvc}
+                              </p>
+                            )}
+                          </>
+                        )}
+                      </InputMask>
+                    </div>
+                  </div>
+                </form>
+              </PaymentCard>
+            </CSSTransition>
+
+            <div className="w-[300px] xs:w-full text-left mb-6 flex justify-start items-center">
+              <input
+                type="checkbox"
+                id="pixRadio"
+                name="paymentMethod"
+                className="mr-2 group"
+                checked={paymentMethod === "pix"}
+                onChange={() => setPaymentMethod("pix")}
+              />
+              <label htmlFor="pixRadio" className="cursor-pointer checkbox">
+                Pagar com PIX
+              </label>
+            </div>
+
+            <CSSTransition
+              in={paymentMethod === "pix"}
+              timeout={300}
+              classNames="payment-transition"
+              unmountOnExit
+            >
+              <PaymentPix>
+                <div className="flex items-center justify-center mt-2 mb-5">
+                  <QRCode value={pixQRCodeData} size={150} />
+                </div>
+                <div className="text-center mt-4 flex justify-center">
+                  <FaRegCopy className="mr-2" />
+                  <button
+                    onClick={copyToClipboard}
+                    className="mb-4 rounded-full font-bold"
+                  >
+                    Copiar código Pix
+                  </button>
+                </div>
+              </PaymentPix>
+            </CSSTransition>
+
+            <div className="w-[300px] xs:w-full text-left mb-6 flex justify-start items-center">
+              <input
+                type="checkbox"
+                id="depositRadio"
+                name="paymentMethod"
+                className="mr-2 group"
+                checked={paymentMethod === "deposit"}
+                onChange={() => setPaymentMethod("deposit")}
+              />
+              <label htmlFor="depositRadio" className="cursor-pointer checkbox">
+                Pagar por transferência bancária
+              </label>
+            </div>
+
+            <CSSTransition
+              in={paymentMethod === "deposit"}
+              timeout={300}
+              classNames="payment-transition"
+              unmountOnExit
+            >
+              <PaymentDeposit />
+            </CSSTransition>
+          </div>
 
           <div className="text-center mt-4">
             <button
--- src/app/payment/style.tsx
+++ src/app/payment/style.tsx
@@ -1,37 +1,47 @@
 import React from "react";
 
 const PaymentContainer = ({ children }) => (
-  <div className="bg-[#eadcd3] bg-opacity-50 mt-[130px] mx-auto rounded-[20px] p-10 shadow-lg flex flex-col items-center justify-center w-1/2 sm:w-5/6 xs:w-5/6 mb-[50px]">
+  <div className="bg-[#eadcd3] bg-opacity-50 mt-[130px] mx-auto rounded-[20px] p-10 shadow-lg flex flex-col items-center justify-center w-1/2 sm:w-5/6 xs:w-[90%] mb-[50px]">
     {children}
   </div>
 );
 
 const PaymentCard = ({ children }) => (
-  <div className="flex flex-col items-center">{children}</div>
+  <div className="flex flex-col items-center mb-2">{children}</div>
 );
 
 const Input = ({ ...props }) => (
   <input
     {...props}
-    className="w-full h-9 mb-2 pl-5 rounded-[50px] text-lg placeholder-gray-400"
+    className="w-full xs:w-[90%] h-9 mb-2 pl-5 rounded-[50px] text-lg placeholder-gray-400"
   />
 );
 
 const ExpiryInput = ({ ...props }) => (
   <input
     {...props}
-    className="w-[166px] h-9 mb-2 pl-5 rounded-[50px] text-lg placeholder-gray-400"
+    className="w-[166px] xs:w-[145px] h-9 mb-2 pl-5 rounded-[50px] text-lg placeholder-gray-400"
   />
 );
 
 const CVCInput = ({ ...props }) => (
   <input
     {...props}
-    className="w-[110px] h-9 mb-2 pl-5 rounded-[50px] text-lg placeholder-gray-400 ml-1"
+    className="w-[110px] xs:w-[100px] h-9 mb-2 pl-5 rounded-[50px] text-lg placeholder-gray-400 ml-1"
   />
 );
 
 const PaymentPix = ({ children }) => <div className="mb-4 mt-4">{children}</div>;
+
+const PaymentDeposit = () => (
+  <div className="border border-gray-300 rounded-lg mb-4 text-left">
+    <h3 className="font-normal text-base mb-2 uppercase">Dados bancários:</h3>
+    <p className="mb-2"><strong>Banco:</strong> Itaú</p>
+    <p className="mb-2"><strong>Agência:</strong> 1234</p>
+    <p className="mb-2"><strong>Conta Corrente:</strong> 567890-1</p>
+    <p className="mb-2"><strong>Nome do Titular:</strong> Pólen Inovação</p>
+  </div>
+);
 
 export {
   PaymentContainer,
@@ -40,4 +50,5 @@ export {
   ExpiryInput,
   CVCInput,
   PaymentPix,
+  PaymentDeposit
 };
