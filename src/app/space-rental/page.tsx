dff253398e63d473a315480f82494537bd11f38e
leticiagouvea <letfgouvea@gmail.com>
2024-08-03 00:01:30 -0300

    style: adjustments to space rental images

--- src/app/space-rental/page.tsx
+++ src/app/space-rental/page.tsx
@@ -11,50 +11,56 @@ import { FaCheck } from "react-icons/fa";
 
 export default function SpaceRentalPage() {
   return (
-    <div className="bg-[#eadcd3] bg-opacity-50 mt-[130px] mx-auto rounded-[20px] p-10 shadow-lg flex flex-col items-center justify-center w-3/5 lg:w-4/6 xs:w-5/6 mb-[50px]">
-      <h1 className="font-bold text-center text-xl mb-8">
+    <div className="bg-[#eadcd3] bg-opacity-50 mt-[120px] mx-auto rounded-[20px] p-10 sm:p-6 shadow-lg flex flex-col items-center justify-center w-3/5 lg:w-4/6 sm:w-5/6 mb-[50px]">
+      <h1 className="font-bold text-center text-xl mb-6">
         Escolha o espaço que deseja reservar:
       </h1>
 
-      <div className="w-full flex flex-wrap justify-center items-center lg:flex-nowrap xl:flex-nowrap lg:space-x-4">
+      <div className="w-full flex flex-wrap sm:flex-col justify-center items-center lg:flex-nowrap xl:flex-nowrap lg:space-x-4">
         <Link href={"/space-rental/auditorium"}>
-          <div className="w-56 h-48 bg-[#EE7A3C] bg-opacity-70 rounded-2xl p-5 flex flex-col justify-center items-center m-2 cursor-pointer hover:bg-opacity-100 hvr-float transition duration-500 ease-out">
+          <div className="w-52 h-[250px] xs:w-[260px] xs:h-[150px] sm:w-[380px] sm:h-[220px] rounded-[10px] m-2 sm:m-0 sm:mb-6 cursor-pointer bg-[rgba(238,122,60,0.5)] shadow-md hvr-push overflow-hidden">
             <Image
-              className="w-full h-36 rounded-md object-cover overflow-hidden"
+              className="w-full h-full shadow-md object-cover overflow-hidden relative opacity-80"
               src={Auditorium}
               alt="Auditório"
             />
-            <h2 className="text-white text-xl mt-2">Auditório</h2>
-            <h3>R$ 80/hora</h3>
+            <div className="w-full absolute bottom-0 left-0 text-[#eadcd3] bg-[rgba(43,43,43,0.5)] flex flex-col items-center p-4 hover:bg-[rgba(234,94,83,0.5)] transition duration-500 ease-out">
+              <h2 className="text-[20px] font-semibold mb-2">Auditório</h2>
+              <h3>R$ 80/hora</h3>
+            </div>
           </div>
         </Link>
 
         <Link href={"/space-rental/tv-studio"}>
-          <div className="w-56 h-48 bg-[#EE7A3C] bg-opacity-70 rounded-2xl p-5 flex flex-col justify-center items-center m-2 cursor-pointer hover:bg-opacity-100 hvr-float transition duration-500 ease-out">
+          <div className="w-52 h-[250px] xs:w-[260px] xs:h-[150px] sm:w-[380px] sm:h-[220px] rounded-[10px] m-2 sm:m-0 sm:mb-6 cursor-pointer bg-[rgba(238,122,60,0.5)] shadow-md hvr-push overflow-hidden">
             <Image
-              className="w-full h-36 rounded-md object-cover overflow-hidden"
+              className="w-full h-full shadow-md object-cover overflow-hidden relative opacity-80"
               src={TVStudio}
               alt="TV Studio"
             />
-            <h2 className="text-white text-xl mt-2">Studio de TV</h2>
-            <h3>R$ 80/hora</h3>
+            <div className="w-full absolute bottom-0 left-0 text-[#eadcd3] bg-[rgba(43,43,43,0.5)] flex flex-col items-center p-4 hover:bg-[rgba(234,94,83,0.5)] transition duration-500 ease-out">
+              <h2 className="text-[20px] font-semibold mb-2">TV Studio</h2>
+              <h3>R$ 80/hora</h3>
+            </div>
           </div>
         </Link>
 
         <Link href={"/space-rental/podcast-studio"}>
-          <div className="w-56 h-48 bg-[#EE7A3C] bg-opacity-70 rounded-2xl p-5 flex flex-col justify-center items-center m-2 cursor-pointer hover:bg-opacity-100 hvr-float transition duration-500 ease-out">
+          <div className="w-52 h-[250px] xs:w-[260px] xs:h-[150px] sm:w-[380px] sm:h-[220px] rounded-[10px] m-2 sm:m-0 cursor-pointer bg-[rgba(238,122,60,0.5)] shadow-md hvr-push overflow-hidden">
             <Image
-              className="w-full h-36 rounded-md object-cover overflow-hidden"
+              className="w-full h-full shadow-md object-cover overflow-hidden relative opacity-80"
               src={PodcastStudio}
               alt="Studio de Podcast"
             />
-            <h2 className="text-white text-xl mt-2">Studio de Podcast</h2>
-            <h3>R$ 80/hora</h3>
+            <div className="w-full absolute bottom-0 left-0 text-[#eadcd3] bg-[rgba(43,43,43,0.5)] flex flex-col items-center p-4 hover:bg-[rgba(234,94,83,0.5)] transition duration-500 ease-out">
+              <h2 className="text-[20px] font-semibold mb-2">Podcast Studio</h2>
+              <h3>R$ 80/hora</h3>
+            </div>
           </div>
         </Link>
       </div>
-      <div className="flex sm:flex-col mt-10 sm:mt-4 justify-center items-center">
-        <div className="flex flex-col sm:flex-row mr-8 sm:mr-0 sm:mb-4">
+      <div className="flex sm:flex-col mt-6 justify-center items-center">
+        <div className="flex flex-col sm:flex-row mr-6 sm:mr-0 sm:mb-4">
           <Image src={SpaceIcon1} alt="Icon 1" className="w-28" />
           <Image src={SpaceIcon2} alt="Icon 2" className="w-28" />
         </div>
